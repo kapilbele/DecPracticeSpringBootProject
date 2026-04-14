d5b04ea03d7c03177dd9ec89ddb22a23c071015e
kapilbele <motirambele06@gmail.com>
2026-04-14 18:06:44 +0530

    adding second commit

--- src/main/java/com/Preparation/SpringBootProject/service/StudentService.java
+++ src/main/java/com/Preparation/SpringBootProject/service/StudentService.java
@@ -6,11 +6,8 @@ import com.Preparation.SpringBootProject.repository.StudentRepo;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-import java.util.Optional;
 
 
 @Service
